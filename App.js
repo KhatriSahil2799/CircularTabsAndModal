f14adfeb093ab8af3b73eb481931a7384b1347e8
KhatriSahil2799 <khatrisahil2799@gmail.com>
2023-07-27 15:10:43 +0530

    Manipulating  cards positions

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
 import {
   GestureDetector,
@@ -23,29 +24,8 @@ const Card = ({ text }) => {
     <View
       style={{
         width: width,
-        // flex: 1,
-        height: 400,
+        height: 500,
         backgroundColor: useGenerateRandomColor(),
-      }}
-    >
-      <Text>{text}</Text>
-    </View>
-  );
-};
-
-const CardB1 = ({ text }) => {
-  return (
-    <View
-      style={{
-        width: width,
-        // flex: 1,
-        height: 400,
-        // position: "absolute",
-        backgroundColor: useGenerateRandomColor(),
-        top: 50,
-        // left: 5,
-
-        // zIndex: 10,
       }}
     >
       <Text>{text}</Text>
@@ -63,43 +43,102 @@ const pivotIncrement = () => {
 };
 
 export default function App() {
-  const CardA = <Card text={"cardA"} key={"cardA"} />;
-  const CardB = <Card text={"cardB"} key={"cardB"} />;
-  const CardC = <Card text={"cardC"} key={"cardC"} />;
+  const [pivot, setPivot] = useState(0);
+
+  const [cardA, setCardA] = useState(<Card text={"cardA"} key={"cardA"} />);
+
+  // let CardA = <Card text={"cardA"} key={"cardA"} />;
+  let CardB = <Card text={"cardB"} key={"cardB"} />;
+  let CardC = <Card text={"cardC"} key={"cardC"} />;
+
+  // useEffect(() => {
+  //   setCardA(<Card text={"cardD"} key={"cardD"} />);
+  //   console.log("🚀 ~ file: App.js:57 ~ useEffect ~ setCardA:");
+  // }, [pivot]);
 
   const END_POSITION = width;
 
   const cardAPosition = useSharedValue(0);
-  const cardAPanOffset = useSharedValue(0);
+  const cardAPanOffset = useSharedValue(-width + 20);
+
+  const cardBPosition = useSharedValue(0);
+  const cardBPanOffset = useSharedValue(0);
+
+  const cardCPosition = useSharedValue(0);
+  const cardCPanOffset = useSharedValue(width - 20);
 
   const panGesture = Gesture.Pan()
     .onUpdate((e) => {
-      position.value = panOffset.value + e.translationX;
+      cardAPosition.value = cardAPanOffset.value + e.translationX;
+      cardBPosition.value = cardBPanOffset.value + e.translationX;
+      cardCPosition.value = cardCPanOffset.value + e.translationX;
     })
     /**
      * to persist the previous scroll position
      */
     .onEnd((e) => {
-      panOffset.value = panOffset.value + e.translationX;
+      // cardAPanOffset.value = cardAPanOffset.value + e.translationX;
+      // cardBPanOffset.value = cardBPanOffset.value + e.translationX;
+      // cardCPanOffset.value = cardCPanOffset.value + e.translationX;
 
-      if (Number(panOffset.value / width) < 0) {
-        runOnJS(pivotIncrement)();
+      if (Math.abs(e.translationX) >= width / 3) {
+        if (e.translationX < 0) {
+          cardBPosition.value = withTiming(-END_POSITION, { duration: 100 });
+          cardBPanOffset.value = -END_POSITION;
+          cardCPosition.value = withTiming(0, { duration: 100 });
+          cardCPanOffset.value = 0;
+          cardAPosition.value = withTiming(END_POSITION, { duration: 100 });
+          cardAPanOffset.value = END_POSITION;
+        }
+        if (e.translationX > 0) {
+          cardBPosition.value = withTiming(END_POSITION, { duration: 100 });
+          cardBPanOffset.value = END_POSITION;
+          cardAPosition.value = withTiming(0, { duration: 100 });
+          cardAPanOffset.value = 0;
+          cardCPosition.value = withTiming(-END_POSITION, { duration: 100 });
+          cardCPanOffset.value = END_POSITION;
+        }
+      } else {
+        cardBPosition.value = withTiming(cardBPanOffset.value, {
+          duration: 100,
+        });
+        // cardAPanOffset.value = cardAPanOffset.value;
+        // cardBPanOffset.value = cardBPanOffset.value;
+        // cardCPanOffset.value = cardCPanOffset.value;
       }
 
-      if (position.value < END_POSITION / 2) {
-        position.value = withTiming(-END_POSITION * 1, { duration: 100 });
-        panOffset.value = -END_POSITION;
-      }
+      // if (Number(cardAPanOffset.value / width) < 0) {
+      //   runOnJS(pivotIncrement)();
+      // }
+
+      // runOnJS(setPivot)(1);
+
+      // if (cardAPosition.value < END_POSITION / 2) {
+      //   cardAPosition.value = withTiming(-END_POSITION * 1, { duration: 100 });
+      //   cardAPanOffset.value = -END_POSITION;
+      // }
     });
 
-  const cardAStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: position.value }],
-  }));
-  const cardBStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: position.value }],
-  }));
+  const cardAStyle = useAnimatedStyle(() => {
+    // console.log(
+    //   "🚀 ~ file: App.js:119 ~ cardAStyle ~ cardAPosition.value :",
+    //   cardAPosition.value
+    // );
+    return {
+      transform: [{ translateX: cardAPosition.value }],
+    };
+  });
+  const cardBStyle = useAnimatedStyle(() => {
+    console.log(
+      "🚀 ~ file: App.js:133 ~ cardBStyle ~ cardBPosition.value :",
+      cardBPosition.value
+    );
+    return {
+      transform: [{ translateX: cardBPosition.value }],
+    };
+  });
   const cardCStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: position.value }],
+    transform: [{ translateX: cardCPosition.value }],
   }));
 
   return (
@@ -109,15 +148,25 @@ export default function App() {
       <GestureDetector gesture={panGesture}>
         <View
           style={{
-            flexDirection: "row",
+            flex: 1,
+            width: "100%",
+            // flexDirection: "row",
             // paddingHorizontal: 20,
-            // backgroundColor: "red",
-            overflow: "scroll",
+            backgroundColor: "yellow",
+            // overflow: "scroll",
           }}
         >
-          <Animated.View style={cardAStyle}>{CardA}</Animated.View>
-          <Animated.View style={cardBStyle}>{CardB}</Animated.View>
-          <Animated.View style={cardCStyle}>{CardC}</Animated.View>
+          {/* <Animated.View style={[styles.absoluteA, cardAStyle]}>
+            {cardA}
+          </Animated.View> */}
+
+          <Animated.View style={[styles.absoluteB, cardBStyle]}>
+            {CardB}
+          </Animated.View>
+
+          {/* <Animated.View style={[styles.absoluteC, cardCStyle]}>
+            {CardC}
+          </Animated.View> */}
         </View>
       </GestureDetector>
     </View>
@@ -130,5 +179,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  absoluteB: {
+    position: "absolute",
+    // top: 0,
+    // left: 0,
+    // right: 100,
+    bottom: 100,
+    // zIndex: 10,
+  },
+  absoluteA: {
+    position: "absolute",
+    // top: 0,
+    // left: 0,
+    // right: width - 20,
+    bottom: 100,
+    // zIndex: 10,
+  },
+  absoluteC: {
+    position: "absolute",
+    // top: 0,
+    // left: width - 20,
+    // right: 100,
+    bottom: 100,
+    // zIndex: 10,
   },
 });
